805982ff3c84d1308e2ec9629e46eb480bfc3d1d
albertosevillaCS <117744863+albertosevillaCS@users.noreply.github.com>
2022-11-15 02:23:51 +0800

    Add files via upload

---
+++ OdeToFood.Core/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Core
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(80)] //-- Sample of DataAnnotations that needs to be met
+        public string Name { get; set; }
+
+        [Required, StringLength(255)]
+        public string Location { get; set; }
+
+
+        public CuisineType Cuisine { get; set; }
+    }
+}
